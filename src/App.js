c811fe082951764676414930931c4e400b2f617a
user <user@DESKTOP-MDJ1E39>
2020-08-09 20:50:23 +0300

    Raduim

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Raduim from 'raduim';
+import Raduim from 'radium';
 import "./App.css";
 import Person from "./Person/Person";
 
@@ -106,4 +106,4 @@ class App extends Component {
   // );
 }
 
-export default Radium(App);
+export default Raduim(App);
