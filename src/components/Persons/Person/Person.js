0eee42d3ca3f86e6d431c5dda369263cbcfffb58
user <user@DESKTOP-MDJ1E39>
2020-08-15 01:00:09 +0300

    Array

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -4,18 +4,16 @@ import classes from "./Person.module.css";
 class Person extends Component {
   render() {
     console.log("[Person.js] redering...");
-    return (
-      <div className={classes.Person}>
-        <p onClick={this.props.click}>
+    return [
+        <p key='i1' onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
-        </p>
-        <p>{this.props.children}</p>
-        <input 
+        </p>,
+        <p key='i2' >{this.props.children}</p>,
+        <input key='i3'
           type="text" 
           onChange={this.props.changed} 
           value={this.props.name} />
-      </div>
-    );
+    ];
   }
 }
 
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,53 +1,60 @@
-import React, { Component } from 'react';
-import Person from './Person/Person';
-import ErrorBoundary from '../ErrorBoundary/ErrorBoundary'
+import React, { PureComponent } from "react";
+import Person from "./Person/Person";
+import ErrorBoundary from "../ErrorBoundary/ErrorBoundary";
 
-class Persons extends Component{
-  static getDerivedStatedFromProps(props, state){
-    console.log('[Persons.js] getDerivedStateFromProps');
-    return state;
+class Persons extends PureComponent {
+  // static getDerivedStatedFromProps(props, state){
+  //   console.log('[Persons.js] getDerivedStateFromProps');
+  //   return state;
+  // }
+
+  // componentWillReceiveProps(props){
+  //   console.log('[Persons.js] componentWillReceiveProps', props);
+  // }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log("[Persons.js] shouldComponentUpdate");
+  //   if (
+  //     nextProps.persons !== this.props.persons ||
+  //     nextProps.changed !== this.props.changed ||
+  //     nextProps.clicked !== this.props.clicked
+  //   ) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  //   // return true;
+  // }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("[Persons.js] getSnapshotBeforeUpdate");
+    return { message: "Snapshot!" };
   }
 
-  componentWillReceiveProps(props){
-    console.log('[Persons.js] componentWillReceiveProps', props);
-  }
-
-  shouldComponentUpdate(nextProps,nextState){
-    console.log('[Persons.js] shouldComponentUpdate');
-    if (nextProps.persons !== this.props.persons){
-      return true;
-    }else{
-      return false;
-    }
-    // return true;
-  }
-
-  getSnapshotBeforeUpdate(prevProps,prevState){
-    console.log('[Persons.js] getSnapshotBeforeUpdate');
-    return { message: 'Snapshot!' };
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot){
-    console.log('[Persons.js] componentDidUpdate');
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("[Persons.js] componentDidUpdate");
     console.log(snapshot);
   }
 
-  componentWillUnmount(){
-    console.log('[Persons.js] componentWillUnmount');
+  componentWillUnmount() {
+    console.log("[Persons.js] componentWillUnmount");
   }
 
-  render(){
-  console.log('[Persons.js] rendering...');
-  return this.props.persons.map((person, index) => {
-    return <ErrorBoundary key={person.id}>
-                <Person
-                click={() => this.props.clicked(index)}
-                name={person.name}
-                age={person.age}
-                changed={(event) => this.props.changed(event, person.id)}/>
-            </ErrorBoundary>
-  });
-};
+  render() {
+    console.log("[Persons.js] rendering...");
+    return this.props.persons.map((person, index) => {
+      return (
+        <ErrorBoundary key={person.id}>
+          <Person
+            click={() => this.props.clicked(index)}
+            name={person.name}
+            age={person.age}
+            changed={(event) => this.props.changed(event, person.id)}
+          />
+        </ErrorBoundary>
+      );
+    });
+  }
 }
 
 export default Persons;
