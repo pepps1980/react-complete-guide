9a7b2b9a143e06fef20c3b6313bf88f5c1e11367
user <user@DESKTOP-MDJ1E39>
2020-08-15 12:18:50 +0300

    useContext

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,25 +6,29 @@ import Auxilliary from "../../../hoc/Auxilliary";
 import AuthContext from "../../../context/auth-context";
 
 class Person extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.inputElementRef = React.createRef();
   }
+
+  static contextType = AuthContext;
 
   componentDidMount() {
     // this.inputElement.focus()
     // document.querySelector("input").focus();
     this.inputElementRef.current.focus();
+    console.log(this.context.authenticated);
   }
 
   render() {
     console.log("[Person.js] redering...");
     return (
       <Auxilliary>
-        <AuthContext.Consumer>
-          {context => context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>}
-        </AuthContext.Consumer>
-        
+        {this.context.authenticated ? (
+          <p>Authenticated!</p>
+        ) : (
+          <p>Please log in</p>
+        )}
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
