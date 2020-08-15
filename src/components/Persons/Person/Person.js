717fcbe62079cf9c322577e99c327b9bab1fb0b6
user <user@DESKTOP-MDJ1E39>
2020-08-15 10:45:06 +0300

    PropTypes

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
 import classes from "./Person.module.css";
 import withClass from '../../../hoc/withClass';
 import Auxilliary from "../../../hoc/Auxilliary";
@@ -21,5 +22,12 @@ class Person extends Component {
     );
   }
 }
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
+};
 
 export default withClass(Person, classes.Person);
