fde0bd81c576adf9082ceb3acd02bf01cb674d2a
user <user@DESKTOP-MDJ1E39>
2020-08-15 18:59:23 +0300

    refresh

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Auxilliary from "../../hoc/Auxilliary";
+import classes from "./Layout.module.css";
+
+
+const layout = (props) => (
+  <Auxilliary>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={classes.Content}>{props.children}</main>
+  </Auxilliary>
+);
+
+export default layout;
